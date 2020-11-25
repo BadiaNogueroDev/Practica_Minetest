139731af58df44ad1581fc4609014552c6b01612
Helnyr <helnyr@users.noreply.github.com>
2020-11-25 20:11:16 +0100

    oricalcum terminado

--- minetest-5.3.0-win64/mods/mymod/init.lua
+++ minetest-5.3.0-win64/mods/mymod/init.lua
@@ -126,3 +126,37 @@ minetest.register_tool("default:sword_orichalcum", {
 	sound = {breaks = "default_tool_breaks"},
 	groups = {sword = 1}
 })
+
+--Spawn del mineral
+minetest.register_ore({
+  ore_type       = "scatter",
+  ore            = "default:stone_with_orichalcum",
+  wherein        = "default:stone",
+  clust_scarcity = 8 * 8 * 8,
+  clust_num_ores = 9,
+  clust_size     = 3,
+  y_max          = 31000,
+  y_min          = 1025,
+})
+
+minetest.register_ore({
+  ore_type       = "scatter",
+  ore            = "default:stone_with_orichalcum",
+  wherein        = "default:stone",
+  clust_scarcity = 8 * 8 * 8,
+  clust_num_ores = 8,
+  clust_size     = 3,
+  y_max          = 64,
+  y_min          = -127,
+})
+
+minetest.register_ore({
+  ore_type       = "scatter",
+  ore            = "default:stone_with_orichalcum",
+  wherein        = "default:stone",
+  clust_scarcity = 12 * 12 * 12,
+  clust_num_ores = 30,
+  clust_size     = 5,
+  y_max          = -128,
+  y_min          = -31000,
+})
